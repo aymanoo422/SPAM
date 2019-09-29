95a2024470a22ed689e9080e573070cbba99e725
aymanoo422 <55666192+aymanoo422@users.noreply.github.com>
2019-09-29 17:51:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("627880182912647224")
+let channel =     client.channels.get("627897686506340389")
 setInterval(function() {
 channel.send(`627880182912647224 قاعد اكرر الكلام مرة و مرة عشان الكريدتس :joy: :yum: :weary: :poop: :joy: :yum: :weary: :poop: :joy: :yum: :weary: :poop: `);
 }, 30)
