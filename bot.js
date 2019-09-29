f42ed6ee38ee8b1e822a001ad057cd0020f879d5
aymanoo422 <55666192+aymanoo422@users.noreply.github.com>
2019-09-29 16:05:51 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("627880182912647224")
+setInterval(function() {
+channel.send(`627880182912647224 قاعد اكرر الكلام مرة و مرة عشان الكريدتس :joy: :yum: :weary: :poop: :joy: :yum: :weary: :poop: :joy: :yum: :weary: :poop: `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
